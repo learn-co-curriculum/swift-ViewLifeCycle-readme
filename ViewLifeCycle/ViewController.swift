edd8096b40ec3b90bcfde29475225402bd6e6a75
JimCampagno <jimcampagno@gmail.com>
2016-09-18 09:08:40 -0400

    Update for Swift 3

--- ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewController.swift
@@ -1,23 +1,33 @@
+//
 //  ViewController.swift
-
+//  ViewLifeCycle
+//
+//  Created by Jim Campagno on 9/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
 
 import UIKit
 
-
 class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad() called")
         view.backgroundColor = UIColor.randomColor()
     }
-
-    override func viewWillAppear(animated: Bool) {
+    
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear(_:) called")
     }
-
-    override func viewDidAppear(animated: Bool) {
+    
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("viewDidAppear(_:) called")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear(_:) called")
     }
 }
